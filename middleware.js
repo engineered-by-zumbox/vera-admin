5009d31e6b9c0740fc3caa1ea3e136ffe372033b
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-02-18 15:00:29 +0100

    update

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const res = NextResponse.next();
+
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "https://veralyssa.vercel.app"); // Allow frontend
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,POST,OPTIONS,DELETE,PUT"
+  );
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type, Authorization"
+  );
+
+  return res;
+}
